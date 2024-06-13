3387dd2e2564aeb1258244d05b31308f0aadc28f
Igor Vinicius <142598070+IgorViniciusPinto@users.noreply.github.com>
2024-06-13 17:23:35 -0300

    Update perfil_usuario.py

--- perfil_usuario.py
+++ perfil_usuario.py
@@ -8,17 +8,17 @@ class PerfilUsuario(ABC):
     _ID_perfil_usuario (int): Identificador único do perfil de usuário.
     _email_perfil_usuario (str): Email do perfil de usuário.
     _senha_perfil_usuario (str): Senha do perfil de usuário.
-    _papel (Optional[str]): Papel do usuário.
+    _cargo (Optional[str]): Cargo do usuário.
     """
     CONTADOR_ID_perfil_usuario:int = 0
 
-    def __init__(self, email:str, senha:int) -> None:
+    def __init__(self, email:str, senha:str) -> None:
         # Inicializa um novo perfil de usuário com email e senha, e atribui um ID único.
         self._ID_perfil_usuario: int = PerfilUsuario.CONTADOR_ID_perfil_usuario
         PerfilUsuario.CONTADOR_ID_perfil_usuario += 1
         self._email_perfil_usuario: str = email
         self._senha_perfil_usuario: str = senha
-        self._papel: Optional[str] = None
+        self._cargo: Optional[str] = None
 
     @abstractmethod
     def consultar_acervo(self, acervo: Any, titulo: Optional[str] = None) -> None:
@@ -37,6 +37,6 @@ class PerfilUsuario(ABC):
     def get_senha_perfil_usuario(self) -> int:
         return self._senha_perfil_usuario
 
-    # Retorna o papel do perfil de usuário.
-    def get_papel_usuario(self) -> Optional[str]:
-        return self._papel
+    # Retorna o cargo do perfil de usuário.
+    def get_cargo_usuario(self) -> Optional[str]:
+        return self._cargo

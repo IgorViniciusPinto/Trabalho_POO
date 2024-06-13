e3ea7ba7224c25302457bbfd448d07c7babc5c6e
Igor Vinicius <142598070+IgorViniciusPinto@users.noreply.github.com>
2024-06-13 07:14:14 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from sistema import Sistema
+
+def main() -> None:
+    # Função principal que inicializa e inicia o sistema.
+    # Cria uma instância da classe Sistema e chama o método inicia_sistema para começar a execução do sistema.
+    sistema = Sistema()
+    sistema.inicia_sistema()
+
+if __name__ == "__main__":
+    main()
